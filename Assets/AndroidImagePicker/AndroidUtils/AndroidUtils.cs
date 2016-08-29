e3d8ca431cb22ee63750fdaa96b946d0a098f6bb
Tibor Kanyó <kanyo.tibi@gmail.com>
2016-08-29 09:41:14 +0200

    First release

---
+++ Assets/AndroidImagePicker/AndroidUtils/AndroidUtils.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public static class AndroidUtils {
+
+    private static AndroidJavaObject currentActivity
+    {
+        get
+        {
+            return new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
+        }
+    }
+	
+    /// <summary>
+    /// Notify the Android system we want to select an image
+    /// </summary>
+    /// <param name="callbackObjectName">Name of gameobject that handles the response</param>
+    /// <param name="callbackMethodName">Name of the callback method on the callback gameobject that handles the response</param>
+    public static void SelectImage(string callbackObjectName, string callbackMethodName)
+    {
+        if (Application.platform != RuntimePlatform.Android)
+            return;
+
+        var jc = new AndroidJavaClass("hu.TiborKanyo.AndroidUtils.ImagePickerRouter");
+        // Calling a Call method to which the current activity is passed, it initializes the activity
+        jc.CallStatic("Call", currentActivity, callbackObjectName, callbackMethodName);
+    }
+}
---
+++ Assets/AndroidImagePicker/ImagePicker/ImagePicker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.ImagePicker
+{
+    public class ImagePicker : MonoBehaviour
+    {
+        /// <summary>
+        /// Default prefix text for the label
+        /// </summary>
+        [SerializeField]
+        private string filePathTextPrefix = "Seletected file: ";
+
+        /// <summary>
+        /// Text component
+        /// </summary>
+        [SerializeField]
+        private Text filePathText;
+
+        /// <summary>
+        /// Show the image picker
+        /// </summary>
+        public void SelectImage()
+        {
+            AndroidUtils.SelectImage("ImagePicker", "OnImageSelected");
+        }
+
+        /// <summary>
+        /// Image picker callback
+        /// </summary>
+        /// <param name="filePath">null if picking was cancelled</param>
+        public void OnImageSelected(string filePath)
+        {
+            //do something meaningful with selected path
+            Debug.Log("OnImageSelected: " + filePath);
+            filePathText.text = filePathTextPrefix + (filePath ?? "null");
+        }
+    }
+}
